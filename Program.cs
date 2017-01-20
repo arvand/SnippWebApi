6cad17a0417ccfaf711f2a843527bbfd85020a9e
Arv <Arv>
2017-01-20 17:24:59 +0330

    a

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace SnippingTool
         static void Main()
         {
             Application.EnableVisualStyles();
+            ;
             Application.SetCompatibleTextRenderingDefault(false);
 
             Application.Run(new Form1());
